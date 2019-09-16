5f5c2d99532c3b6a82bf0e3d7d3082c6a07a99ac
Luis-12 <alfred,va98@gmail.com>
2019-09-15 20:03:55 -0600

    prueba notificacion

--- src/main/java/s/c/m/entities/Departamento.java
+++ src/main/java/s/c/m/entities/Departamento.java
@@ -4,5 +4,7 @@ public class Departamento {
     private String nombre;
     private  String descripcion;
     private String fk_idadmin;
+    private String estado;
+    private String pk_coddepartamento;
 
 }
