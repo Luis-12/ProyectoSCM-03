cbdf4e72bfc15d76132b5be77815477f9ffde9ed
Yoselyn24 <yoselyn2411@gmail.com>
2019-09-16 01:21:09 -0500

    se arregla la ubicacion de la carpeta
    Se agregó el header con el logo y el footer
    
    carpeta imagen
    
    se agrega carpeta Beans, dentro se crea DepartamentoBean y se incluyen los campos para crear un departamento

---
+++ src/main/java/beans/DepartamentoBean.java
@@ -0,0 +1,14 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import java.util.List;
+
+@Component
+public class DepartamentoBean {
+
+
+}
